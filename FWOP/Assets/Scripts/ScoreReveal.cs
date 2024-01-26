83d4ecafe77e1407dc5125b559fee130f55bd9d3
Daniel Korsah <danielkorsah97@gmail.com>
2024-01-26 22:32:12 +0000

    Add callbacks

--- FWOP/Assets/Scripts/ScoreReveal.cs
+++ FWOP/Assets/Scripts/ScoreReveal.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ScoreReveal : MonoBehaviour
 {
+    private UnityAction onDiveEnd;
     // Start is called before the first frame update
-    void Start()
+    void Enable()
+    {
+        onDiveEnd += ScoreDive;
+    }
+
+    // Update is called once per frame
+    void ScoreDive()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        onDiveEnd -= ScoreDive;
     }
 }
