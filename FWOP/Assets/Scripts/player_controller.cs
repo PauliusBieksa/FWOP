7d38e8db8db4059209d161e610f137dd9ac79f13
Paulius <pauliuspaulius@hotmail.com>
2024-01-27 22:55:19 +0000

    started starting sequence

--- FWOP/Assets/Scripts/player_controller.cs
+++ FWOP/Assets/Scripts/player_controller.cs
@@ -26,27 +26,48 @@ public class player_controller : MonoBehaviour
     public HingeJoint2D foot1_object;
     public HingeJoint2D foot2_object;
 
+    private float animation_legth;
+
     // Start is called before the first frame update
     void Start()
     {
+        JointMotor2D mot_clk = new JointMotor2D();
+        JointMotor2D mot_cclk = new JointMotor2D();
+        mot_clk.maxMotorTorque = 100;
+        mot_cclk.maxMotorTorque = 100;
+        mot_clk.motorSpeed = player_joint_force;
+        mot_cclk.motorSpeed = -player_joint_force;
+        shoulder1_object.motor = mot_clk;
+        shoulder2_object.motor = mot_clk;
+        elbow1_object.motor = mot_cclk;
+        elbow2_object.motor = mot_cclk;
+        hip1_object.motor = mot_cclk;
+        hip2_object.motor = mot_cclk;
+        knee1_object.motor = mot_clk;
+        knee2_object.motor = mot_clk;
+        foot1_object.motor = mot_cclk;
+        foot2_object.motor = mot_cclk;
+
+        animation_legth = animation_timer;
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (pre_jump)
         {
-            JointMotor2D mot_clk = new JointMotor2D();
-            JointMotor2D mot_cclk = new JointMotor2D();
-            shoulder1_object.motor = mot_cclk;
-            shoulder2_object.motor = mot_cclk;
-            elbow1_object.motor = mot_clk;
-            elbow2_object.motor = mot_clk;
-            hip1_object.motor = mot_clk;
-            hip2_object.motor = mot_clk;
-            knee1_object.motor = mot_cclk;
-            knee2_object.motor = mot_cclk;
-            foot1_object.motor = mot_clk;
-            foot2_object.motor = mot_clk;
+            if (pre_animation_timer <= 0)
+            {
+                if (animation_timer <= 0) pre_jump = false;
+                animation_timer -= Time.deltaTime;
+
+                transform.position = Vector3.Lerp(animation_start_pos, animation_end_pos, 1 - (animation_timer / animation_legth));
+                player.angularVelocity = -220;
+            }
+            else
+            {
+                transform.position = animation_start_pos;
+            }
+            pre_animation_timer -= Time.deltaTime;
         }
         else
         {
