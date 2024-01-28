854589ebecdcbb2e6dc30b1854a1548ad0c351d1
Daniel Korsah <danielkorsah97@gmail.com>
2024-01-28 14:24:53 +0000

    Retry LoadingBar. Prefab the canvas.

--- FWOP/Assets/Scripts/Retry.cs
+++ FWOP/Assets/Scripts/Retry.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Retry : MonoBehaviour
 {
+    public GameObject loadingBar;
+    public Image loadingBarFill;
+    public float HoldTime;
+
+    private float timeHeld;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKey(KeyCode.R))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            loadingBar.SetActive(true);
+            timeHeld += Time.deltaTime;
+            loadingBarFill.fillAmount = timeHeld/HoldTime;
+            
+            if(timeHeld > HoldTime)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            loadingBar.SetActive(false);
+            timeHeld = 0;
         }
     }
 }
