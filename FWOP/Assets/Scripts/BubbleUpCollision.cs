2a6f0ffaa94d8e2db104b6ebfb2bc3e45aee279e
Daniel Korsah <danielkorsah97@gmail.com>
2024-01-28 11:54:28 +0000

    Player Sounds and triggers. Pull Many other sounds.

---
+++ FWOP/Assets/Scripts/BubbleUpCollision.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+
+public class BubbleUpCollision : MonoBehaviour
+{
+    private Collider2D parentCollider;
+    void Start()
+    {
+        parentCollider = GetComponentInParent<Collider2D>();
+    }
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        parentCollider.O
+    }
+}
+
+
+
+    
---
+++ FWOP/Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerSounds : MonoBehaviour
+{
+    public AudioSource AudioSource;
+
+    public List<AudioClip> Screams;
+    public List<AudioClip> Splashes;
+    public List<AudioClip> Splats;
+    
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (AudioSource.isPlaying)
+        {
+            return;
+        }
+        
+        if (!AudioSource.isPlaying && other.gameObject.CompareTag("Obstacle"))
+        {
+            AudioSource.clip = Screams[Random.Range(0, Screams.Count-1)];
+            AudioSource.Play();
+        }
+        
+        if (other.gameObject.CompareTag("Pool"))
+        {
+            AudioSource.clip = Splashes[Random.Range(0, Splashes.Count-1)];
+            AudioSource.Play();
+        }
+        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            AudioSource.clip = Splats[Random.Range(0, Splats.Count-1)];
+            AudioSource.Play();
+        }
+        
+    }
+}
